eabc1969c1e16b8db887a0a7eee199c4e09da6df
mrjoelee <joe.05.lee@outlook.com>
2022-11-04 11:50:36 -0400

    Added Board, Player Class, Conf Folder

---
+++ src/com/testyourjavabeans/Board.java
@@ -0,0 +1,31 @@
+package com.testyourjavabeans;
+import java.io.Serializable;
+import java.nio.file.Path;
+
+class Board implements Serializable {
+    //fields
+    private static final String questionPath = null;
+    private static final String answerPath = null;
+
+    //business methods
+    private Board(){
+        //TODO getinstance()
+    }
+
+    private void save(){
+
+    }
+
+    private void update(){
+
+    }
+
+    private void show(){
+        //level, questions
+    }
+    /*
+     * Collections of Questions and Answer Map: K: (Integer)Question# V: (String)Questions itself
+     * Map K: (Integer)Answer# V: (String) Answers
+     */
+
+}
