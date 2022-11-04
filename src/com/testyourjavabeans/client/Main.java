3140c993126d34e99b8db4d35d86f1b6477014b3
Sarina Lyons <sarinalyons@gmail.com>
2022-11-04 14:25:39 -0400

    Added map method in Board

---
+++ src/com/testyourjavabeans/client/Main.java
@@ -0,0 +1,10 @@
+package com.testyourjavabeans.client;
+import com.testyourbeans.app.TriviaApp;
+
+
+class Main {
+    public static void main(String[] args) {
+        TriviaApp app = new TriviaApp();
+        app.execute();
+    }
+}
